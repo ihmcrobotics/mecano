3dcd268afcacc323274a7c6b1f5e12de4c4df622
Sylvain Bertrand <sbertrand@ihmc.us>
2018-11-06 17:31:21 -0600

    Removing desired accelerations, started w/ SpatialAccelCalculator.
    Removed Joint.getDesiredJointAcceleration(SpatialAccel).
    
    Removed setter for desired accleration.
    
    Removed getter for desired accleration.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/DifferentialIDMassMatrixCalculator.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/DifferentialIDMassMatrixCalculator.java
@@ -64,7 +64,7 @@ public class DifferentialIDMassMatrixCalculator implements MassMatrixCalculator
       for (int i = 0 ; i < totalNumberOfDoFs; i++)
       {
          tmpDesiredJointAccelerationsMatrix.set(i, 0, 1.0);
-         ScrewTools.setDesiredAccelerations(jointsInOrder, tmpDesiredJointAccelerationsMatrix);
+         ScrewTools.setJointAccelerations(jointsInOrder, tmpDesiredJointAccelerationsMatrix);
          
          idCalculator.compute();
          tmpTauMatrix.set(ScrewTools.getTauMatrix(jointsInOrder));
@@ -81,14 +81,14 @@ public class DifferentialIDMassMatrixCalculator implements MassMatrixCalculator
    {
       for (JointBasics joint : jointsInOrder)
       {
-         joint.setDesiredAccelerationToZero();
+         joint.setJointAccelerationToZero();
          joint.setJointVelocity(0, new DenseMatrix64F(joint.getDegreesOfFreedom(), 1));
       }
    }
 
    private void storeJointState()
    {
-      ScrewTools.getDesiredJointAccelerationsMatrix(jointsInOrder, storedJointDesiredAccelerations);
+      ScrewTools.getJointAccelerationsMatrix(jointsInOrder, storedJointDesiredAccelerations);
       ScrewTools.getJointVelocitiesMatrix(jointsInOrder, storedJointVelocities);
       for (JointBasics joint : jointsInOrder)
       {
@@ -106,7 +106,7 @@ public class DifferentialIDMassMatrixCalculator implements MassMatrixCalculator
    
    private void restoreJointState()
    {
-      ScrewTools.setDesiredAccelerations(jointsInOrder, storedJointDesiredAccelerations);
+      ScrewTools.setJointAccelerations(jointsInOrder, storedJointDesiredAccelerations);
       ScrewTools.setVelocities(jointsInOrder, storedJointVelocities);
       
       for (JointBasics joint : jointsInOrder)
