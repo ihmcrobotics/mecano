c2847bb4215c44bb26c14a384e9ea86853d96dce
Sylvain Bertrand <sbertrand@ihmc.us>
2021-12-07 08:55:26 -0600

    Switched constructor and fixed some bug with updating the YoVariables.

--- src/yovariables/java/us/ihmc/mecano/yoVariables/multiBodySystem/YoCrossFourBarJoint.java
+++ src/yovariables/java/us/ihmc/mecano/yoVariables/multiBodySystem/YoCrossFourBarJoint.java
@@ -1,7 +1,11 @@
 package us.ihmc.mecano.yoVariables.multiBodySystem;
 
+import us.ihmc.euclid.matrix.interfaces.Matrix3DReadOnly;
+import us.ihmc.euclid.orientation.interfaces.Orientation3DReadOnly;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.mecano.multiBodySystem.CrossFourBarJoint;
-import us.ihmc.mecano.multiBodySystem.interfaces.RevoluteJointBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
 
@@ -12,9 +16,50 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    private final YoDouble velocityLimitLower, velocityLimitUpper;
    private final YoDouble effortLimitLower, effortLimitUpper;
 
-   public YoCrossFourBarJoint(String name, RevoluteJointBasics[] fourBarJoints, int actuatedJointIndex, YoRegistry registry)
+   public YoCrossFourBarJoint(String name,
+                              RigidBodyBasics predecessor,
+                              String jointNameA,
+                              String jointNameB,
+                              String jointNameC,
+                              String jointNameD,
+                              String bodyNameDA,
+                              String bodyNameBC,
+                              RigidBodyTransformReadOnly transformAToPredecessor,
+                              RigidBodyTransformReadOnly transformBToPredecessor,
+                              RigidBodyTransformReadOnly transformCToB,
+                              RigidBodyTransformReadOnly transformDToA,
+                              Matrix3DReadOnly bodyInertiaDA,
+                              Matrix3DReadOnly bodyInertiaBC,
+                              double bodyMassDA,
+                              double bodyMassBC,
+                              RigidBodyTransformReadOnly bodyInertiaPoseDA,
+                              RigidBodyTransformReadOnly bodyInertiaPoseBC,
+                              int actuatedJointIndex,
+                              int loopClosureJointIndex,
+                              Vector3DReadOnly jointAxis,
+                              YoRegistry registry)
    {
-      super(name, fourBarJoints, actuatedJointIndex);
+      super(name,
+            predecessor,
+            jointNameA,
+            jointNameB,
+            jointNameC,
+            jointNameD,
+            bodyNameDA,
+            bodyNameBC,
+            transformAToPredecessor,
+            transformBToPredecessor,
+            transformCToB,
+            transformDToA,
+            bodyInertiaDA,
+            bodyInertiaBC,
+            bodyMassDA,
+            bodyMassBC,
+            bodyInertiaPoseDA,
+            bodyInertiaPoseBC,
+            actuatedJointIndex,
+            loopClosureJointIndex,
+            jointAxis);
 
       q = new YoDouble("q_" + getName(), registry);
       qd = new YoDouble("qd_" + getName(), registry);
@@ -26,13 +71,23 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
       velocityLimitUpper = new YoDouble("qd_max_" + getName(), registry);
       effortLimitLower = new YoDouble("tau_min_" + getName(), registry);
       effortLimitUpper = new YoDouble("tau_max_" + getName(), registry);
+
+      q.addListener(v -> super.setQ(q.getValue()));
+      qd.addListener(v -> super.setQd(qd.getValue()));
+      qdd.addListener(v -> super.setQdd(qdd.getValue()));
+      tau.addListener(v -> super.setTau(tau.getValue()));
+      jointLimitLower.addListener(v -> super.setJointLimitLower(jointLimitLower.getValue()));
+      jointLimitUpper.addListener(v -> super.setJointLimitUpper(jointLimitUpper.getValue()));
+      velocityLimitLower.addListener(v -> super.setVelocityLimitLower(velocityLimitLower.getValue()));
+      velocityLimitUpper.addListener(v -> super.setVelocityLimitUpper(velocityLimitUpper.getValue()));
+      effortLimitLower.addListener(v -> super.setEffortLimitLower(effortLimitLower.getValue()));
+      effortLimitUpper.addListener(v -> super.setEffortLimitUpper(effortLimitUpper.getValue()));
    }
 
    /** {@inheritDoc} */
    @Override
    public void setQ(double q)
    {
-      super.setQ(q);
       this.q.set(q);
    }
 
@@ -40,7 +95,6 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public void setQd(double qd)
    {
-      super.setQd(qd);
       this.qd.set(qd);
    }
 
@@ -48,7 +102,6 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public void setQdd(double qdd)
    {
-      super.setQdd(qdd);
       this.qdd.set(qdd);
    }
 
@@ -56,34 +109,70 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public void setTau(double tau)
    {
-      super.setTau(tau);
       this.tau.set(tau);
    }
 
-   /** {@inheritDoc} */
    @Override
-   public void setJointLimits(double jointLimitLower, double jointLimitUpper)
+   public void setJointOrientation(Orientation3DReadOnly jointOrientation)
    {
-      super.setJointLimits(jointLimitLower, jointLimitUpper);
+      super.setJointOrientation(jointOrientation);
+      getQ(); // Update YoVariable
+   }
+
+   @Override
+   public void setJointAngularVelocity(Vector3DReadOnly jointAngularVelocity)
+   {
+      super.setJointAngularVelocity(jointAngularVelocity);
+      getQd(); // Update YoVariable
+   }
+
+   @Override
+   public void setJointAngularAcceleration(Vector3DReadOnly jointAngularAcceleration)
+   {
+      super.setJointAngularAcceleration(jointAngularAcceleration);
+      getQdd(); // Update YoVariable
+   }
+
+   @Override
+   public void setJointTorque(Vector3DReadOnly jointTorque)
+   {
+      super.setJointTorque(jointTorque);
+      getTau(); // Update YoVariable
+   }
+
+   @Override
+   public void setJointLimitLower(double jointLimitLower)
+   {
       this.jointLimitLower.set(jointLimitLower);
+   }
+
+   @Override
+   public void setJointLimitUpper(double jointLimitUpper)
+   {
       this.jointLimitUpper.set(jointLimitUpper);
    }
 
-   /** {@inheritDoc} */
    @Override
-   public void setVelocityLimits(double velocityLimitLower, double velocityLimitUpper)
+   public void setVelocityLimitLower(double velocityLimitLower)
    {
-      super.setVelocityLimits(velocityLimitLower, velocityLimitUpper);
       this.velocityLimitLower.set(velocityLimitLower);
+   }
+
+   @Override
+   public void setVelocityLimitUpper(double velocityLimitUpper)
+   {
       this.velocityLimitUpper.set(velocityLimitUpper);
    }
 
-   /** {@inheritDoc} */
    @Override
-   public void setEffortLimits(double effortLimitLower, double effortLimitUpper)
+   public void setEffortLimitLower(double effortLimitLower)
    {
-      super.setEffortLimits(effortLimitLower, effortLimitUpper);
       this.effortLimitLower.set(effortLimitLower);
+   }
+
+   @Override
+   public void setEffortLimitUpper(double effortLimitUpper)
+   {
       this.effortLimitUpper.set(effortLimitUpper);
    }
 
@@ -91,7 +180,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getQ()
    {
-      q.set(super.getQ());
+      q.set(super.getQ(), false);
       return q.getValue();
    }
 
@@ -99,7 +188,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getQd()
    {
-      qd.set(super.getQd());
+      qd.set(super.getQd(), false);
       return qd.getValue();
    }
 
@@ -107,7 +196,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getQdd()
    {
-      qdd.set(super.getQdd());
+      qdd.set(super.getQdd(), false);
       return qdd.getValue();
    }
 
@@ -115,7 +204,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getTau()
    {
-      tau.set(super.getTau());
+      tau.set(super.getTau(), false);
       return tau.getValue();
    }
 
@@ -123,7 +212,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getJointLimitLower()
    {
-      jointLimitLower.set(super.getJointLimitLower());
+      jointLimitLower.set(super.getJointLimitLower(), false);
       return jointLimitLower.getValue();
    }
 
@@ -131,7 +220,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getJointLimitUpper()
    {
-      jointLimitUpper.set(super.getJointLimitUpper());
+      jointLimitUpper.set(super.getJointLimitUpper(), false);
       return jointLimitUpper.getValue();
    }
 
@@ -139,7 +228,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getVelocityLimitLower()
    {
-      velocityLimitLower.set(super.getVelocityLimitLower());
+      velocityLimitLower.set(super.getVelocityLimitLower(), false);
       return velocityLimitLower.getValue();
    }
 
@@ -147,7 +236,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getVelocityLimitUpper()
    {
-      velocityLimitUpper.set(super.getVelocityLimitUpper());
+      velocityLimitUpper.set(super.getVelocityLimitUpper(), false);
       return velocityLimitUpper.getValue();
    }
 
@@ -155,7 +244,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getEffortLimitLower()
    {
-      effortLimitLower.set(super.getEffortLimitLower());
+      effortLimitLower.set(super.getEffortLimitLower(), false);
       return effortLimitLower.getValue();
    }
 
@@ -163,7 +252,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    @Override
    public double getEffortLimitUpper()
    {
-      effortLimitUpper.set(super.getEffortLimitUpper());
+      effortLimitUpper.set(super.getEffortLimitUpper(), false);
       return effortLimitUpper.getValue();
    }
 }
