30e6529130d4753eaaf846c37588871efde42949
Sylvain Bertrand <sbertrand@ihmc.org>
2021-12-10 10:51:19 -0600

    Added documentation to YoCrossFourBarJoint.

--- src/yovariables/java/us/ihmc/mecano/yoVariables/multiBodySystem/YoCrossFourBarJoint.java
+++ src/yovariables/java/us/ihmc/mecano/yoVariables/multiBodySystem/YoCrossFourBarJoint.java
@@ -11,6 +11,12 @@ import us.ihmc.mecano.tools.MultiBodySystemFactories.RigidBodyBuilder;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
 
+/**
+ * Implementation of a {@code CrossFourBarJoint} with internal state variables backed with
+ * {@code YoVariable}s.
+ * 
+ * @see CrossFourBarJoint
+ */
 public class YoCrossFourBarJoint extends CrossFourBarJoint
 {
    private final YoDouble q, qd, qdd, tau;
@@ -18,6 +24,74 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
    private final YoDouble velocityLimitLower, velocityLimitUpper;
    private final YoDouble effortLimitLower, effortLimitUpper;
 
+   /**
+    * Creates a new cross four bar joint with the following structure:
+    * 
+    * <pre>
+    *    root
+    *      |
+    *      |
+    * A O-----O B
+    *    \   /
+    *     \ /
+    *      X
+    *     / \
+    *    /   \
+    * C O-----O D
+    *      |
+    * end-effector
+    * </pre>
+    * <p>
+    * Internally, this joint creates a kinematics that represents the above diagram using revolute
+    * joints and rigid-bodies. The kinematics is isolated from the kinematics that this cross four bar
+    * joint is connected to. This internal kinematics is only used to facilitates computation of this
+    * joint properties.
+    * </p>
+    * <p>
+    * Note that the mass properties of the two cross bars, i.e. rigid bodies DA and BC, are not used in
+    * the rigid-body algorithms such as inverse and forward dynamics calculators.
+    * </p>
+    * 
+    * @param name                    the name of this joint.
+    * @param predecessor             the rigid-body connected to and preceding this joint.
+    * @param jointNameA              the name of the joint A, see diagram above. Can be {@code null}.
+    * @param jointNameB              the name of the internal joint B, see diagram above. Can be
+    *                                {@code null}.
+    * @param jointNameC              the name of the internal joint C, see diagram above. Can be
+    *                                {@code null}.
+    * @param jointNameD              the name of the internal joint D, see diagram above. Can be
+    *                                {@code null}.
+    * @param bodyNameDA              the name of the internal rigid body DA, i.e. cross bar, see
+    *                                diagram above. Can be {@code null}.
+    * @param bodyNameBC              the name of the internal rigid body BC, i.e. cross bar, see
+    *                                diagram above. Can be {@code null}.
+    * @param transformAToPredecessor the transform from the frame after the parent joint to the joint
+    *                                A. Not modified.
+    * @param transformBToPredecessor the transform from the frame after the parent joint to the joint
+    *                                B. Not modified.
+    * @param transformDToA           the transform from the frame after the joint A to the joint D. Not
+    *                                modified.
+    * @param transformCToB           the transform from the frame after the joint B to the joint C. Not
+    *                                modified.
+    * @param bodyInertiaDA           the 3D momentum inertia of the rigid-body DA. Can be {@code null}.
+    * @param bodyInertiaBC           the 3D momentum inertia of the rigid-body BC. Can be {@code null}.
+    * @param bodyMassDA              the mass of the rigid-body DA. Can be {@code 0.0}.
+    * @param bodyMassBC              the mass of the rigid-body BC. Can be {@code 0.0}.
+    * @param bodyInertiaPoseDA       the transform of the rigid-body DA's body-fixed frame. Can be
+    *                                {@code null}.
+    * @param bodyInertiaPoseBC       the transform of the rigid-body BC's body-fixed frame. Can be
+    *                                {@code null}.
+    * @param actuatedJointIndex      the index of the joint that is actuated, i.e. torque source. 0 is
+    *                                for joint A, 1 for B, 2 for C, and 3 for D.
+    * @param loopClosureJointIndex   the index of the joint is constructed as the loop closure of the
+    *                                internal mechanism. Can be 2 for joint C or 3 for joint D. It has
+    *                                to be different from {@code actuatedJointIndex}. The frame after
+    *                                the other joint is used as the frame after this cross four bar
+    *                                joint.
+    * @param jointAxis               the axis of this joint. It is also used to define the axis of the
+    *                                internal revolute joints.
+    * @param registry                the registry to register child variables to.
+    */
    public YoCrossFourBarJoint(String name,
                               RigidBodyBasics predecessor,
                               String jointNameA,
@@ -66,6 +140,76 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
            registry);
    }
 
+   /**
+    * Creates a new cross four bar joint with the following structure:
+    * 
+    * <pre>
+    *    root
+    *      |
+    *      |
+    * A O-----O B
+    *    \   /
+    *     \ /
+    *      X
+    *     / \
+    *    /   \
+    * C O-----O D
+    *      |
+    * end-effector
+    * </pre>
+    * <p>
+    * Internally, this joint creates a kinematics that represents the above diagram using revolute
+    * joints and rigid-bodies. The kinematics is isolated from the kinematics that this cross four bar
+    * joint is connected to. This internal kinematics is only used to facilitates computation of this
+    * joint properties.
+    * </p>
+    * <p>
+    * Note that the mass properties of the two cross bars, i.e. rigid bodies DA and BC, are not used in
+    * the rigid-body algorithms such as inverse and forward dynamics calculators.
+    * </p>
+    * 
+    * @param name                    the name of this joint.
+    * @param predecessor             the rigid-body connected to and preceding this joint.
+    * @param jointNameA              the name of the joint A, see diagram above. Can be {@code null}.
+    * @param jointNameB              the name of the internal joint B, see diagram above. Can be
+    *                                {@code null}.
+    * @param jointNameC              the name of the internal joint C, see diagram above. Can be
+    *                                {@code null}.
+    * @param jointNameD              the name of the internal joint D, see diagram above. Can be
+    *                                {@code null}.
+    * @param bodyNameDA              the name of the internal rigid body DA, i.e. cross bar, see
+    *                                diagram above. Can be {@code null}.
+    * @param bodyNameBC              the name of the internal rigid body BC, i.e. cross bar, see
+    *                                diagram above. Can be {@code null}.
+    * @param transformAToPredecessor the transform from the frame after the parent joint to the joint
+    *                                A. Not modified.
+    * @param transformBToPredecessor the transform from the frame after the parent joint to the joint
+    *                                B. Not modified.
+    * @param transformDToA           the transform from the frame after the joint A to the joint D. Not
+    *                                modified.
+    * @param transformCToB           the transform from the frame after the joint B to the joint C. Not
+    *                                modified.
+    * @param bodyInertiaDA           the 3D momentum inertia of the rigid-body DA. Can be {@code null}.
+    * @param bodyInertiaBC           the 3D momentum inertia of the rigid-body BC. Can be {@code null}.
+    * @param bodyMassDA              the mass of the rigid-body DA. Can be {@code 0.0}.
+    * @param bodyMassBC              the mass of the rigid-body BC. Can be {@code 0.0}.
+    * @param bodyInertiaPoseDA       the transform of the rigid-body DA's body-fixed frame. Can be
+    *                                {@code null}.
+    * @param bodyInertiaPoseBC       the transform of the rigid-body BC's body-fixed frame. Can be
+    *                                {@code null}.
+    * @param rigidBodyBuilder        the factory used to construct the two cross bars, i.e.
+    *                                rigid-bodies DA and BC.
+    * @param actuatedJointIndex      the index of the joint that is actuated, i.e. torque source. 0 is
+    *                                for joint A, 1 for B, 2 for C, and 3 for D.
+    * @param loopClosureJointIndex   the index of the joint is constructed as the loop closure of the
+    *                                internal mechanism. Can be 2 for joint C or 3 for joint D. It has
+    *                                to be different from {@code actuatedJointIndex}. The frame after
+    *                                the other joint is used as the frame after this cross four bar
+    *                                joint.
+    * @param jointAxis               the axis of this joint. It is also used to define the axis of the
+    *                                internal revolute joints.
+    * @param registry                the registry to register child variables to.
+    */
    public YoCrossFourBarJoint(String name,
                               RigidBodyBasics predecessor,
                               String jointNameA,
@@ -164,6 +308,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
       this.tau.set(tau);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setJointOrientation(Orientation3DReadOnly jointOrientation)
    {
@@ -171,6 +316,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
       getQ(); // Update YoVariable
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setJointAngularVelocity(Vector3DReadOnly jointAngularVelocity)
    {
@@ -178,6 +324,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
       getQd(); // Update YoVariable
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setJointAngularAcceleration(Vector3DReadOnly jointAngularAcceleration)
    {
@@ -185,6 +332,7 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
       getQdd(); // Update YoVariable
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setJointTorque(Vector3DReadOnly jointTorque)
    {
@@ -192,36 +340,42 @@ public class YoCrossFourBarJoint extends CrossFourBarJoint
       getTau(); // Update YoVariable
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setJointLimitLower(double jointLimitLower)
    {
       this.jointLimitLower.set(jointLimitLower);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setJointLimitUpper(double jointLimitUpper)
    {
       this.jointLimitUpper.set(jointLimitUpper);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setVelocityLimitLower(double velocityLimitLower)
    {
       this.velocityLimitLower.set(velocityLimitLower);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setVelocityLimitUpper(double velocityLimitUpper)
    {
       this.velocityLimitUpper.set(velocityLimitUpper);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setEffortLimitLower(double effortLimitLower)
    {
       this.effortLimitLower.set(effortLimitLower);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setEffortLimitUpper(double effortLimitUpper)
    {
