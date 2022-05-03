79f1a6a606e79833aef89ddeb3ee8604bb924934
Sylvain Bertrand <sbertrand@ihmc.org>
2022-05-03 16:12:53 -0500

    Mess

--- src/main/java/us/ihmc/mecano/algorithms/MultiBodyGravityGradientCalculator.java
+++ src/main/java/us/ihmc/mecano/algorithms/MultiBodyGravityGradientCalculator.java
@@ -9,13 +9,18 @@ import org.ejml.data.DMatrixRMaj;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameTuple3DReadOnly;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
 import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.mecano.multiBodySystem.interfaces.JointReadOnly;
 import us.ihmc.mecano.multiBodySystem.interfaces.MultiBodySystemReadOnly;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyReadOnly;
 import us.ihmc.mecano.spatial.SpatialInertia;
 import us.ihmc.mecano.spatial.Twist;
+import us.ihmc.mecano.spatial.interfaces.TwistBasics;
+import us.ihmc.mecano.spatial.interfaces.TwistReadOnly;
 import us.ihmc.mecano.tools.MultiBodySystemTools;
 
 public class MultiBodyGravityGradientCalculator
@@ -23,6 +28,7 @@ public class MultiBodyGravityGradientCalculator
    private final MultiBodySystemReadOnly input;
    private final DMatrixRMaj gradientMatrix;
    private final AlgorithmStep initialStep;
+   private final FrameVector3D gravitationalAcceleration = new FrameVector3D();
 
    public MultiBodyGravityGradientCalculator(MultiBodySystemReadOnly input)
    {
@@ -33,6 +39,72 @@ public class MultiBodyGravityGradientCalculator
       initialStep = new AlgorithmStep(input.getRootBody(), null, null);
       buildMultiBodyTree(initialStep, input.getJointsToIgnore());
       initialStep.includeIgnoredSubtreeInertia();
+
+      gravitationalAcceleration.setToZero(input.getInertialFrame());
+   }
+
+   /**
+    * Set the gravitational acceleration to account for in this multi-body system.
+    * <p>
+    * The acceleration of the root body is set to the opposite of the gravitational acceleration such
+    * that it gets naturally propagated to the whole system.
+    * </p>
+    *
+    * @param gravity the gravitational linear acceleration, it is usually equal to
+    *                {@code (0, 0, -9.81)}.
+    */
+   public void setGravitionalAcceleration(FrameTuple3DReadOnly gravity)
+   {
+      gravitationalAcceleration.set(gravity);
+   }
+
+   /**
+    * Set the gravitational acceleration to account for in this multi-body system.
+    * <p>
+    * The acceleration of the root body is set to the opposite of the gravitational acceleration such
+    * that it gets naturally propagated to the whole system.
+    * </p>
+    *
+    * @param gravity the gravitational linear acceleration, it is usually equal to
+    *                {@code (0, 0, -9.81)}.
+    */
+   public void setGravitionalAcceleration(Tuple3DReadOnly gravity)
+   {
+      gravitationalAcceleration.setIncludingFrame(input.getInertialFrame(), gravity);
+   }
+
+   /**
+    * Set the gravitational acceleration to account for in this multi-body system.
+    * <p>
+    * The acceleration of the root body is set to the opposite of the gravitational acceleration such
+    * that it gets naturally propagated to the whole system.
+    * </p>
+    *
+    * @param gravity the gravitational linear acceleration along the z-axis, it is usually equal to
+    *                {@code -9.81}.
+    */
+   public void setGravitionalAcceleration(double gravity)
+   {
+      setGravitionalAcceleration(0.0, 0.0, gravity);
+   }
+
+   /**
+    * Set the gravitational acceleration to account for in this multi-body system.
+    * <p>
+    * The acceleration of the root body is set to the opposite of the gravitational acceleration such
+    * that it gets naturally propagated to the whole system.
+    * </p>
+    *
+    * @param gravityX the gravitational linear acceleration along the x-axis, it is usually equal to
+    *                 {@code 0}.
+    * @param gravityY the gravitational linear acceleration along the y-axis, it is usually equal to
+    *                 {@code 0}.
+    * @param gravityZ the gravitational linear acceleration along the z-axis, it is usually equal to
+    *                 {@code -9.81}.
+    */
+   public void setGravitionalAcceleration(double gravityX, double gravityY, double gravityZ)
+   {
+      gravitationalAcceleration.setIncludingFrame(input.getInertialFrame(), gravityX, gravityY, gravityZ);
    }
 
    private List<AlgorithmStep> buildMultiBodyTree(AlgorithmStep parent, Collection<? extends JointReadOnly> jointsToIgnore)
@@ -107,8 +179,9 @@ public class MultiBodyGravityGradientCalculator
       private final FramePoint3D centerOfMass = new FramePoint3D();
 
       private final FramePoint3D childCoM = new FramePoint3D();
-      private final Twist[] unitTwists;
       private final FrameVector3D gravity = new FrameVector3D();
+
+      private final RigidBodyTransform transformToInertial = new RigidBodyTransform();
 
       public AlgorithmStep(RigidBodyReadOnly rigidBody, AlgorithmStep parent, int[] jointIndices)
       {
@@ -119,15 +192,11 @@ public class MultiBodyGravityGradientCalculator
          if (isRoot())
          {
             bodyInertia = null;
-            unitTwists = null;
          }
          else
          {
             parent.children.add(this);
             bodyInertia = new SpatialInertia(rigidBody.getInertia());
-            unitTwists = new Twist[jointIndices.length];
-            for (int i = 0; i < jointIndices.length; i++)
-               unitTwists[i] = new Twist();
          }
       }
 
@@ -150,6 +219,8 @@ public class MultiBodyGravityGradientCalculator
             children.get(childIndex).includeIgnoredSubtreeInertia();
       }
 
+      private final Twist unitTwist_i = new Twist();
+
       public void passOne()
       {
          for (int i = 0; i < children.size(); i++)
@@ -161,8 +232,10 @@ public class MultiBodyGravityGradientCalculator
             subTreeMass += children.get(i).subTreeMass;
 
          ReferenceFrame frameToUse = isRoot() ? getBodyFixedFrame() : getFrameAfterJoint();
-         gravity.setIncludingFrame(frameToUse.getRootFrame(), 0.0, 0.0, -10.0); // TODO
-         gravity.changeFrame(frameToUse);
+         transformToInertial.set(frameToUse.getTransformToRoot());
+         gravity.setIncludingFrame(gravitationalAcceleration);
+         transformToInertial.inverseTransform(gravity);
+         gravity.setReferenceFrame(frameToUse);
 
          // The centerOfMassTimesMass can be used to obtain the overall center of mass position.
          if (bodyInertia == null)
@@ -174,11 +247,6 @@ public class MultiBodyGravityGradientCalculator
             centerOfMassTimesMass.setIncludingFrame(bodyInertia.getCenterOfMassOffset());
             centerOfMassTimesMass.changeFrame(frameToUse);
             centerOfMassTimesMass.scale(bodyInertia.getMass());
-
-            for (int i = 0; i < getNumberOfDoFs(); i++)
-            {
-               unitTwists[i].setIncludingFrame(getJoint().getUnitTwists().get(i));
-            }
          }
 
          for (int i = 0; i < children.size(); i++)
@@ -205,25 +273,31 @@ public class MultiBodyGravityGradientCalculator
 
          for (int i = 0; i < getNumberOfDoFs(); i++)
          {
-            Twist unitTwist_i = unitTwists[i];
-            unitTwists[i].setIncludingFrame(getJoint().getUnitTwists().get(i));
+            TwistReadOnly unitTwist_i = getUnitTwist(i);
             tempCross.cross(gravity, unitTwist_i.getAngularPart());
-            tempCross.cross(centerOfMassTimesMass);
-            double gradient_ii = tempCross.dot(unitTwist_i.getAngularPart());
+            double gradient_ii = -tempCross.dot(unitTwist_i.getLinearPart());
+            tempCross.cross(centerOfMass);
+            gradient_ii += tempCross.dot(unitTwist_i.getAngularPart());
+            gradient_ii *= subTreeMass;
             gradientMatrix.set(jointIndices[i], jointIndices[i], gradient_ii);
 
             for (int j = 0; j < i; j++)
             {
-               Twist unitTwist_j = unitTwists[j];
+               TwistReadOnly unitTwist_j = getUnitTwist(j);
                tempCross.cross(gravity, unitTwist_j.getAngularPart());
-               tempCross.cross(centerOfMassTimesMass);
-               double gradient_ji = tempCross.dot(unitTwist_i.getAngularPart());
+               double gradient_ji = -tempCross.dot(unitTwist_i.getLinearPart());
+               tempCross.cross(gravity, unitTwist_j.getAngularPart());
+               tempCross.cross(centerOfMass);
+               gradient_ji += tempCross.dot(unitTwist_i.getAngularPart());
+               gradient_ji *= subTreeMass;
                gradientMatrix.set(jointIndices[j], jointIndices[i], gradient_ji);
 
-               tempCross.cross(gravity, unitTwist_i.getAngularPart());
-               tempCross.cross(centerOfMassTimesMass);
-               double gradient_ij = tempCross.dot(unitTwist_j.getAngularPart());
-               gradientMatrix.set(jointIndices[i], jointIndices[j], gradient_ij);
+//               tempCross.cross(gravity, unitTwist_i.getAngularPart());
+//               double gradient_ij = -subTreeMass * tempCross.dot(unitTwist_j.getLinearPart());
+//               tempCross.cross(centerOfMass);
+//               gradient_ij += tempCross.dot(unitTwist_j.getAngularPart());
+//               gradient_ij *= subTreeMass;
+//               gradientMatrix.set(jointIndices[i], jointIndices[j], gradient_ij);
             }
          }
 
@@ -234,17 +308,17 @@ public class MultiBodyGravityGradientCalculator
             for (int j = 0; j < getNumberOfDoFs(); j++)
             {
                int index_j = jointIndices[j];
-               Twist unitTwist_j = unitTwists[j];
+               TwistReadOnly unitTwist_j = getUnitTwist(j);
 
                for (int i = 0; i < ancestor.getNumberOfDoFs(); i++)
                {
                   int index_i = ancestor.jointIndices[i];
-                  Twist unitTwist_i = ancestor.unitTwists[i];
+                  getUnitTwistInLocalFrame(ancestor, i, unitTwist_i);
 
-                  unitTwist_i.changeFrame(getFrameAfterJoint());
                   tempCross.cross(gravity, unitTwist_i.getAngularPart());
+                  double gradient_ij = -subTreeMass * tempCross.dot(unitTwist_j.getLinearPart());
                   tempCross.cross(centerOfMassTimesMass);
-                  double gradient_ij = tempCross.dot(unitTwist_j.getAngularPart());
+                  gradient_ij += tempCross.dot(unitTwist_j.getAngularPart());
                   gradientMatrix.set(index_i, index_j, gradient_ij);
                   gradientMatrix.set(index_j, index_i, gradient_ij);
                }
@@ -252,6 +326,13 @@ public class MultiBodyGravityGradientCalculator
 
             ancestor = ancestor.parent;
          }
+      }
+
+      private void getUnitTwistInLocalFrame(AlgorithmStep other, int dofIndex, TwistBasics unitTwistToPack)
+      {
+         unitTwistToPack.setIncludingFrame(other.getUnitTwist(dofIndex));
+         unitTwistToPack.applyTransform(other.transformToInertial);
+         unitTwistToPack.applyInverseTransform(transformToInertial);
       }
 
       public boolean isRoot()
@@ -272,6 +353,11 @@ public class MultiBodyGravityGradientCalculator
       public MovingReferenceFrame getFrameAfterJoint()
       {
          return getJoint().getFrameAfterJoint();
+      }
+
+      public TwistReadOnly getUnitTwist(int dofIndex)
+      {
+         return getJoint().getUnitTwists().get(dofIndex);
       }
 
       public MovingReferenceFrame getBodyFixedFrame()
--- src/test/java/us/ihmc/mecano/algorithms/MultiBodyGravityGradientCalculatorTest.java
+++ src/test/java/us/ihmc/mecano/algorithms/MultiBodyGravityGradientCalculatorTest.java
@@ -1,6 +1,5 @@
 package us.ihmc.mecano.algorithms;
 
-import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
@@ -9,9 +8,16 @@ import org.ejml.dense.row.CommonOps_DDRM;
 import org.junit.jupiter.api.Test;
 
 import us.ihmc.mecano.algorithms.TablePrinter.Alignment;
+import us.ihmc.mecano.multiBodySystem.interfaces.CrossFourBarJointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.FixedJointReadOnly;
 import us.ihmc.mecano.multiBodySystem.interfaces.JointBasics;
 import us.ihmc.mecano.multiBodySystem.interfaces.JointReadOnly;
 import us.ihmc.mecano.multiBodySystem.interfaces.MultiBodySystemBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.PlanarJointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.PrismaticJointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.RevoluteJointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.SixDoFJointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.SphericalJointReadOnly;
 import us.ihmc.mecano.tools.JointStateType;
 import us.ihmc.mecano.tools.MecanoTestTools;
 import us.ihmc.mecano.tools.MultiBodySystemRandomTools;
@@ -24,7 +30,7 @@ public class MultiBodyGravityGradientCalculatorTest
    private static final double GRAVITY = 10.0;
 
    @Test
-   public void testCalculator()
+   public void testCalculatorRevoluteJointChain()
    {
       Random random = new Random(2342356);
 
@@ -35,38 +41,99 @@ public class MultiBodyGravityGradientCalculatorTest
       {
          int numberOfJoints = 5;
          List<? extends JointBasics> joints = MultiBodySystemRandomTools.nextRevoluteJointChain(random, numberOfJoints);
-         MultiBodySystemBasics input = MultiBodySystemBasics.toMultiBodySystemBasics(joints);
-
-         MultiBodySystemRandomTools.nextState(random, JointStateType.CONFIGURATION, joints);
-         MultiBodySystemRandomTools.nextState(random, JointStateType.VELOCITY, joints);
-         input.getRootBody().updateFramesRecursively();
-
-         DMatrixRMaj expectedGradient = computeGradientByFD(input, dq);
-         MultiBodyGravityGradientCalculator calculator = new MultiBodyGravityGradientCalculator(input);
-         calculator.compute();
-         DMatrixRMaj actualGradient = calculator.getGradientMatrix();
-
-         try
-         {
-            MecanoTestTools.assertDMatrixEquals(expectedGradient, actualGradient, epsilon);
-         }
-         catch (Throwable e)
-         {
-            //            TablePrinter tablePrinter = new TablePrinter();
-            //            int col = 0;
-            //            tablePrinter.setCell(0, col, "Act. Delta", Alignment.LEFT);
-            //            tablePrinter.setSubTable(1, col++, actualDeltaGravity);
-            //            tablePrinter.setCell(0, col, "Exp. Delta", Alignment.LEFT);
-            //            tablePrinter.setSubTable(1, col++, expectedDeltaGravity);
-            //            tablePrinter.setCell(0, col, "Err. Delta", Alignment.LEFT);
-            //            tablePrinter.setSubTable(1, col++, errorDeltaGravity);
-            //            tablePrinter.setCell(0, col, "Act. Gradient", Alignment.LEFT);
-            //            CommonOps_DDRM.scale(dt, gradient);
-            //            tablePrinter.setSubTable(1, col++, gradient);
-            //            System.out.println(tablePrinter);
-            throw e;
-         }
+         compareAgainstFiniteDifference(random, joints, dq, epsilon, i);
       }
+   }
+   
+   @Test
+   public void testCalculatorOneDoFJointChain()
+   {
+      Random random = new Random(2342356);
+      
+      double dq = 1.0e-7;
+      double epsilon = 1.0e-5;
+      
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         int numberOfJoints = 5;
+         List<? extends JointBasics> joints = MultiBodySystemRandomTools.nextOneDoFJointChain(random, numberOfJoints);
+         compareAgainstFiniteDifference(random, joints, dq, epsilon, i);
+      }
+   }
+
+   @Test
+   public void testCalculatorJointChain()
+   {
+      Random random = new Random(2342350);
+
+      double dq = 1.0e-7;
+      double epsilon = 1.0e-5;
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         int numberOfJoints = 1;
+         List<? extends JointBasics> joints = MultiBodySystemRandomTools.nextJointChain(random, numberOfJoints);
+         compareAgainstFiniteDifference(random, joints, dq, epsilon, i);
+      }
+   }
+   
+   public void compareAgainstFiniteDifference(Random random, List<? extends JointBasics> joints, double dq, double epsilon, int iteration)
+   {
+      MultiBodySystemBasics input = MultiBodySystemBasics.toMultiBodySystemBasics(joints);
+//      MultiBodySystemRandomTools.nextState(random, JointStateType.CONFIGURATION, joints);
+      MultiBodySystemRandomTools.nextState(random, JointStateType.VELOCITY, joints);
+      input.getRootBody().updateFramesRecursively();
+
+      DMatrixRMaj expectedGradient = computeGradientByFD(input, dq);
+      MultiBodyGravityGradientCalculator calculator = new MultiBodyGravityGradientCalculator(input);
+      calculator.setGravitionalAcceleration(-GRAVITY);
+      calculator.compute();
+      DMatrixRMaj actualGradient = calculator.getGradientMatrix();
+
+      try
+      {
+         MecanoTestTools.assertDMatrixEquals("Iteration: " + iteration, expectedGradient, actualGradient, epsilon);
+      }
+      catch (Throwable e)
+      {
+         TablePrinter tablePrinter = new TablePrinter();
+         int col = 1;
+         int row = 1;
+         for (JointBasics joint : input.getJointsToConsider())
+         {
+            for (int j = 0; j < joint.getDegreesOfFreedom(); j++)
+            {
+               String dofName = joint.getName() + " [" + toShortTypeString(joint) + "]";
+               tablePrinter.setCell(0, col++, dofName, Alignment.LEFT);
+               tablePrinter.setCell(row++, 0, dofName, Alignment.LEFT);
+            }
+         }
+         DMatrixRMaj diff = new DMatrixRMaj(input.getNumberOfDoFs(), input.getNumberOfDoFs());
+         CommonOps_DDRM.subtract(expectedGradient, actualGradient, diff);
+         CommonOps_DDRM.abs(diff);
+         tablePrinter.setSubTable(1, 1, diff);
+         System.out.println(tablePrinter);
+         throw e;
+      }
+   }
+
+   private static String toShortTypeString(JointReadOnly joint)
+   {
+      if (joint instanceof RevoluteJointReadOnly)
+         return "R";
+      if (joint instanceof PrismaticJointReadOnly)
+         return "P";
+      if (joint instanceof CrossFourBarJointReadOnly)
+         return "C";
+      if (joint instanceof SphericalJointReadOnly)
+         return "S";
+      if (joint instanceof SixDoFJointReadOnly)
+         return "6";
+      if (joint instanceof FixedJointReadOnly)
+         return "0";
+      if (joint instanceof PlanarJointReadOnly)
+         return "3";
+      return null;
    }
 
    @Test
