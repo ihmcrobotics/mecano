06d2856865002fb55b4fe925bff02c6214daa53c
Sylvain Bertrand <sbertrand@ihmc.org>
2022-05-02 23:07:41 -0500

    New calculator and its test.

---
+++ src/main/java/us/ihmc/mecano/algorithms/MultiBodyGravityGradientCalculator.java
@@ -0,0 +1,260 @@
+package us.ihmc.mecano.algorithms;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.ejml.data.DMatrixRMaj;
+
+import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.euclid.referenceFrame.FrameVector3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.mecano.frames.MovingReferenceFrame;
+import us.ihmc.mecano.multiBodySystem.interfaces.JointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.MultiBodySystemReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyReadOnly;
+import us.ihmc.mecano.spatial.SpatialInertia;
+import us.ihmc.mecano.spatial.Twist;
+import us.ihmc.mecano.tools.MultiBodySystemTools;
+
+public class MultiBodyGravityGradientCalculator
+{
+   private final MultiBodySystemReadOnly input;
+   private final DMatrixRMaj gradientMatrix;
+   private final AlgorithmStep initialStep;
+
+   public MultiBodyGravityGradientCalculator(MultiBodySystemReadOnly input)
+   {
+      this.input = input;
+
+      gradientMatrix = new DMatrixRMaj(input.getNumberOfDoFs(), input.getNumberOfDoFs());
+
+      initialStep = new AlgorithmStep(input.getRootBody(), null, null);
+      buildMultiBodyTree(initialStep, input.getJointsToIgnore());
+      initialStep.includeIgnoredSubtreeInertia();
+   }
+
+   private List<AlgorithmStep> buildMultiBodyTree(AlgorithmStep parent, Collection<? extends JointReadOnly> jointsToIgnore)
+   {
+      List<AlgorithmStep> algorithmSteps = new ArrayList<>();
+      algorithmSteps.add(parent);
+
+      List<JointReadOnly> childrenJoints = new ArrayList<>(parent.rigidBody.getChildrenJoints());
+
+      if (childrenJoints.size() > 1)
+      { // Reorganize the joints in the children to ensure that loop closures are treated last.
+         List<JointReadOnly> loopClosureAncestors = new ArrayList<>();
+
+         for (int i = 0; i < childrenJoints.size();)
+         {
+            if (MultiBodySystemTools.doesSubtreeContainLoopClosure(childrenJoints.get(i).getSuccessor()))
+               loopClosureAncestors.add(childrenJoints.remove(i));
+            else
+               i++;
+         }
+
+         childrenJoints.addAll(loopClosureAncestors);
+      }
+
+      for (JointReadOnly childJoint : childrenJoints)
+      {
+         if (jointsToIgnore.contains(childJoint))
+            continue;
+
+         if (childJoint.isLoopClosure())
+         {
+            /*
+             * We simply skip any loop closure joint which will leave their columns in the matrix set to zero,
+             * which is what we want.
+             */
+            continue;
+         }
+
+         RigidBodyReadOnly childBody = childJoint.getSuccessor();
+         if (childBody != null)
+         {
+            int[] jointIndices = input.getJointMatrixIndexProvider().getJointDoFIndices(childJoint);
+            AlgorithmStep child = new AlgorithmStep(childBody, parent, jointIndices);
+            algorithmSteps.addAll(buildMultiBodyTree(child, jointsToIgnore));
+         }
+      }
+
+      return algorithmSteps;
+   }
+
+   public void compute()
+   {
+      initialStep.passOne();
+   }
+
+   public DMatrixRMaj getGradientMatrix()
+   {
+      return gradientMatrix;
+   }
+
+   private class AlgorithmStep
+   {
+      private final RigidBodyReadOnly rigidBody;
+      private final SpatialInertia bodyInertia;
+      private final AlgorithmStep parent;
+      private final int[] jointIndices;
+      private final List<AlgorithmStep> children = new ArrayList<>();
+
+      private double subTreeMass;
+      private final FramePoint3D centerOfMassTimesMass = new FramePoint3D();
+      private final FramePoint3D centerOfMass = new FramePoint3D();
+
+      private final FramePoint3D childCoM = new FramePoint3D();
+      private final Twist[] unitTwists;
+      private final FrameVector3D gravity = new FrameVector3D();
+
+      public AlgorithmStep(RigidBodyReadOnly rigidBody, AlgorithmStep parent, int[] jointIndices)
+      {
+         this.rigidBody = rigidBody;
+         this.parent = parent;
+         this.jointIndices = jointIndices;
+
+         if (isRoot())
+         {
+            bodyInertia = null;
+            unitTwists = null;
+         }
+         else
+         {
+            parent.children.add(this);
+            bodyInertia = new SpatialInertia(rigidBody.getInertia());
+            unitTwists = new Twist[jointIndices.length];
+            for (int i = 0; i < jointIndices.length; i++)
+               unitTwists[i] = new Twist();
+         }
+      }
+
+      public void includeIgnoredSubtreeInertia()
+      {
+         if (!isRoot() && children.size() != rigidBody.getChildrenJoints().size())
+         {
+            for (JointReadOnly childJoint : rigidBody.getChildrenJoints())
+            {
+               if (input.getJointsToIgnore().contains(childJoint))
+               {
+                  SpatialInertia subtreeIneria = MultiBodySystemTools.computeSubtreeInertia(childJoint);
+                  subtreeIneria.changeFrame(rigidBody.getBodyFixedFrame());
+                  bodyInertia.add(subtreeIneria);
+               }
+            }
+         }
+
+         for (int childIndex = 0; childIndex < children.size(); childIndex++)
+            children.get(childIndex).includeIgnoredSubtreeInertia();
+      }
+
+      public void passOne()
+      {
+         for (int i = 0; i < children.size(); i++)
+            children.get(i).passOne();
+
+
+         // Update the subtree mass
+         subTreeMass = bodyInertia == null ? 0.0 : bodyInertia.getMass();
+         for (int i = 0; i < children.size(); i++)
+            subTreeMass += children.get(i).subTreeMass;
+
+         ReferenceFrame frameToUse = isRoot() ? getBodyFixedFrame() : getFrameAfterJoint();
+         gravity.setIncludingFrame(frameToUse.getRootFrame(), 0.0, 0.0, -10.0); // TODO
+         gravity.changeFrame(frameToUse);
+
+         // The centerOfMassTimesMass can be used to obtain the overall center of mass position.
+         if (bodyInertia == null)
+         {
+            centerOfMassTimesMass.setToZero(getBodyFixedFrame());
+         }
+         else
+         {
+            centerOfMassTimesMass.setIncludingFrame(bodyInertia.getCenterOfMassOffset());
+            centerOfMassTimesMass.changeFrame(frameToUse);
+            centerOfMassTimesMass.scale(bodyInertia.getMass());
+         }
+
+         for (int i = 0; i < children.size(); i++)
+         {
+            childCoM.setIncludingFrame(children.get(i).centerOfMass);
+            childCoM.changeFrame(frameToUse);
+            centerOfMassTimesMass.scaleAdd(children.get(i).subTreeMass, childCoM, centerOfMassTimesMass);
+         }
+
+         centerOfMass.setIncludingFrame(centerOfMassTimesMass);
+         centerOfMass.scale(1.0 / subTreeMass);
+
+         if (!isRoot())
+         {
+            for (int i = 0; i < getJoint().getDegreesOfFreedom(); i++)
+            { // TODO Looks good
+               unitTwists[i].setIncludingFrame(getJoint().getUnitTwists().get(i));
+               tempCross.cross(gravity, unitTwists[i].getAngularPart());
+               tempCross.cross(centerOfMassTimesMass);
+               double gradientElement = tempCross.dot(unitTwists[i].getAngularPart());
+               gradientMatrix.set(jointIndices[i], jointIndices[i], gradientElement);
+
+               for (int j = 0; j < i; j++)
+               { // TODO Looks good
+                  tempCross.cross(gravity, unitTwists[j].getAngularPart());
+                  tempCross.cross(centerOfMassTimesMass);
+                  gradientElement = tempCross.dot(unitTwists[i].getAngularPart());
+                  gradientMatrix.set(jointIndices[j], jointIndices[i], gradientElement);
+
+                  tempCross.cross(gravity, unitTwists[i].getAngularPart());
+                  tempCross.cross(centerOfMassTimesMass);
+                  gradientElement = tempCross.dot(unitTwists[j].getAngularPart());
+                  gradientMatrix.set(jointIndices[i], jointIndices[j], gradientElement);
+               }
+
+               // TODO Following is wrong
+               for (int childIndex = 0; childIndex < children.size(); childIndex++)
+               {
+                  AlgorithmStep child = children.get(i);
+
+                  for (int j = 0; j < child.getJoint().getDegreesOfFreedom(); j++)
+                  {
+                     child.unitTwists[j].changeFrame(getFrameAfterJoint());
+
+                     tempCross.cross(child.gravity, child.unitTwists[j].getAngularPart());
+                     tempCross.cross(child.centerOfMassTimesMass);
+                     gradientElement = tempCross.dot(unitTwists[i].getAngularPart());
+                     gradientMatrix.set(jointIndices[i], child.jointIndices[j], gradientElement);
+//                     gradientMatrix.set(child.jointIndices[j], jointIndices[i], gradientElement);
+                     
+                     tempCross.cross(gravity, unitTwists[i].getAngularPart());
+                     tempCross.cross(centerOfMassTimesMass);
+                     gradientElement = tempCross.dot(child.unitTwists[j].getAngularPart());
+//                     gradientMatrix.set(jointIndices[i], child.jointIndices[j], gradientElement);
+                     gradientMatrix.set(child.jointIndices[j], jointIndices[i], gradientElement);
+                  }
+               }
+            }
+         }
+      }
+
+      private final Vector3D tempCross = new Vector3D();
+
+      public boolean isRoot()
+      {
+         return parent == null;
+      }
+
+      public JointReadOnly getJoint()
+      {
+         return rigidBody.getParentJoint();
+      }
+
+      public MovingReferenceFrame getFrameAfterJoint()
+      {
+         return getJoint().getFrameAfterJoint();
+      }
+
+      public MovingReferenceFrame getBodyFixedFrame()
+      {
+         return rigidBody.getBodyFixedFrame();
+      }
+   }
+}
---
+++ src/test/java/us/ihmc/mecano/algorithms/MultiBodyGravityGradientCalculatorTest.java
@@ -0,0 +1,195 @@
+package us.ihmc.mecano.algorithms;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.CommonOps_DDRM;
+import org.junit.jupiter.api.Test;
+
+import us.ihmc.mecano.algorithms.TablePrinter.Alignment;
+import us.ihmc.mecano.multiBodySystem.interfaces.JointBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.JointReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.MultiBodySystemBasics;
+import us.ihmc.mecano.tools.JointStateType;
+import us.ihmc.mecano.tools.MecanoTestTools;
+import us.ihmc.mecano.tools.MultiBodySystemRandomTools;
+import us.ihmc.mecano.tools.MultiBodySystemStateIntegrator;
+import us.ihmc.mecano.tools.MultiBodySystemTools;
+
+public class MultiBodyGravityGradientCalculatorTest
+{
+   private static final int ITERATIONS = 1000;
+   private static final double GRAVITY = 10.0;
+
+   @Test
+   public void testCalculator()
+   {
+      Random random = new Random(2342356);
+
+      double dq = 1.0e-7;
+      double epsilon = 1.0e-6;
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         int numberOfJoints = 5;
+         List<? extends JointBasics> joints = MultiBodySystemRandomTools.nextRevoluteJointChain(random, numberOfJoints);
+         MultiBodySystemBasics input = MultiBodySystemBasics.toMultiBodySystemBasics(joints);
+
+         MultiBodySystemRandomTools.nextState(random, JointStateType.CONFIGURATION, joints);
+         MultiBodySystemRandomTools.nextState(random, JointStateType.VELOCITY, joints);
+         input.getRootBody().updateFramesRecursively();
+
+         DMatrixRMaj expectedGradient = computeGradientByFD(input, dq);
+         MultiBodyGravityGradientCalculator calculator = new MultiBodyGravityGradientCalculator(input);
+         calculator.compute();
+         DMatrixRMaj actualGradient = calculator.getGradientMatrix();
+
+         try
+         {
+            MecanoTestTools.assertDMatrixEquals(expectedGradient, actualGradient, epsilon);
+         }
+         catch (Throwable e)
+         {
+            //            TablePrinter tablePrinter = new TablePrinter();
+            //            int col = 0;
+            //            tablePrinter.setCell(0, col, "Act. Delta", Alignment.LEFT);
+            //            tablePrinter.setSubTable(1, col++, actualDeltaGravity);
+            //            tablePrinter.setCell(0, col, "Exp. Delta", Alignment.LEFT);
+            //            tablePrinter.setSubTable(1, col++, expectedDeltaGravity);
+            //            tablePrinter.setCell(0, col, "Err. Delta", Alignment.LEFT);
+            //            tablePrinter.setSubTable(1, col++, errorDeltaGravity);
+            //            tablePrinter.setCell(0, col, "Act. Gradient", Alignment.LEFT);
+            //            CommonOps_DDRM.scale(dt, gradient);
+            //            tablePrinter.setSubTable(1, col++, gradient);
+            //            System.out.println(tablePrinter);
+            throw e;
+         }
+      }
+   }
+
+   @Test
+   public void testFiniteDifference()
+   {
+      Random random = new Random(2342356);
+
+      double dt = 1.0e-5;
+      double dq = 1.0e-5;
+      double epsilon = 1.0e-6;
+      MultiBodySystemStateIntegrator integrator = new MultiBodySystemStateIntegrator(dt);
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         int numberOfJoints = 10;
+         List<? extends JointBasics> joints = MultiBodySystemRandomTools.nextRevoluteJointChain(random, numberOfJoints);
+         MultiBodySystemBasics input = MultiBodySystemBasics.toMultiBodySystemBasics(joints);
+
+         DMatrixRMaj qDot = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+         DMatrixRMaj actualDeltaGravity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+         DMatrixRMaj actualGravity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+         DMatrixRMaj expectedDeltaGravity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+         DMatrixRMaj errorDeltaGravity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+         DMatrixRMaj prevGravity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+         DMatrixRMaj expectedGravity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+
+         MultiBodySystemRandomTools.nextState(random, JointStateType.CONFIGURATION, joints);
+         MultiBodySystemRandomTools.nextState(random, JointStateType.VELOCITY, joints);
+         input.getRootBody().updateFramesRecursively();
+
+         DMatrixRMaj gradient = computeGradientByFD(input, dq);
+
+         MultiBodySystemTools.extractJointsState(input.getJointsToConsider(), JointStateType.VELOCITY, qDot);
+         CommonOps_DDRM.mult(gradient, qDot, actualDeltaGravity);
+         CommonOps_DDRM.scale(dt, actualDeltaGravity);
+
+         InverseDynamicsCalculator calculator = new InverseDynamicsCalculator(input);
+         calculator.setConsiderCoriolisAndCentrifugalForces(false);
+         calculator.setConsiderJointAccelerations(false);
+         calculator.setGravitionalAcceleration(-GRAVITY);
+         calculator.compute();
+         prevGravity.set(calculator.getJointTauMatrix());
+         CommonOps_DDRM.add(prevGravity, actualDeltaGravity, actualGravity);
+
+         input.getRootBody().updateFramesRecursively();
+         integrator.integrateFromVelocitySubtree(input.getRootBody());
+         input.getRootBody().updateFramesRecursively();
+         calculator.compute();
+         expectedGravity.set(calculator.getJointTauMatrix());
+
+         CommonOps_DDRM.subtract(expectedGravity, prevGravity, expectedDeltaGravity);
+         CommonOps_DDRM.subtract(expectedDeltaGravity, actualDeltaGravity, errorDeltaGravity);
+
+         try
+         {
+            MecanoTestTools.assertDMatrixEquals(expectedGravity, actualGravity, epsilon);
+            MecanoTestTools.assertDMatrixEquals(expectedDeltaGravity, actualDeltaGravity, epsilon);
+         }
+         catch (Throwable e)
+         {
+            TablePrinter tablePrinter = new TablePrinter();
+            int col = 0;
+            tablePrinter.setCell(0, col, "Act. Delta", Alignment.LEFT);
+            tablePrinter.setSubTable(1, col++, actualDeltaGravity);
+            tablePrinter.setCell(0, col, "Exp. Delta", Alignment.LEFT);
+            tablePrinter.setSubTable(1, col++, expectedDeltaGravity);
+            tablePrinter.setCell(0, col, "Err. Delta", Alignment.LEFT);
+            tablePrinter.setSubTable(1, col++, errorDeltaGravity);
+            tablePrinter.setCell(0, col, "Act. Gradient", Alignment.LEFT);
+            CommonOps_DDRM.scale(dt, gradient);
+            tablePrinter.setSubTable(1, col++, gradient);
+            System.out.println(tablePrinter);
+            throw e;
+         }
+      }
+   }
+
+   private static DMatrixRMaj computeGradientByFD(MultiBodySystemBasics input, double dq)
+   {
+      DMatrixRMaj originalConfiguration = new DMatrixRMaj(input.getJointsToConsider().stream().mapToInt(JointReadOnly::getConfigurationMatrixSize).sum(), 1);
+      MultiBodySystemTools.extractJointsState(input.getJointsToConsider(), JointStateType.CONFIGURATION, originalConfiguration);
+      DMatrixRMaj originalVelocity = new DMatrixRMaj(input.getNumberOfDoFs(), 1);
+      MultiBodySystemTools.extractJointsState(input.getJointsToConsider(), JointStateType.VELOCITY, originalVelocity);
+
+      InverseDynamicsCalculator calculator = new InverseDynamicsCalculator(input);
+      calculator.setConsiderCoriolisAndCentrifugalForces(false);
+      calculator.setConsiderJointAccelerations(false);
+      calculator.setGravitionalAcceleration(-GRAVITY);
+      MultiBodySystemStateIntegrator integrator = new MultiBodySystemStateIntegrator(dq);
+
+      int nDoFs = input.getNumberOfDoFs();
+      DMatrixRMaj qDot = new DMatrixRMaj(nDoFs, 1);
+      DMatrixRMaj gradient = new DMatrixRMaj(nDoFs, nDoFs);
+      DMatrixRMaj gradientCol = new DMatrixRMaj(nDoFs, 1);
+
+      MultiBodySystemTools.insertJointsState(input.getJointsToConsider(), JointStateType.VELOCITY, qDot);
+      input.getRootBody().updateFramesRecursively();
+
+      calculator.compute();
+      DMatrixRMaj referenceGravity = new DMatrixRMaj(calculator.getJointTauMatrix());
+
+      for (int dofIndex = 0; dofIndex < nDoFs; dofIndex++)
+      {
+         qDot.zero();
+         qDot.set(dofIndex, 0, 1);
+         MultiBodySystemTools.insertJointsState(input.getJointsToConsider(), JointStateType.VELOCITY, qDot);
+         input.getRootBody().updateFramesRecursively();
+         integrator.integrateFromVelocitySubtree(input.getRootBody());
+         input.getRootBody().updateFramesRecursively();
+
+         calculator.compute();
+
+         CommonOps_DDRM.subtract(calculator.getJointTauMatrix(), referenceGravity, gradientCol);
+         CommonOps_DDRM.scale(1.0 / dq, gradientCol);
+         CommonOps_DDRM.insert(gradientCol, gradient, 0, dofIndex);
+
+         MultiBodySystemTools.insertJointsState(input.getJointsToConsider(), JointStateType.CONFIGURATION, originalConfiguration);
+      }
+
+      MultiBodySystemTools.insertJointsState(input.getJointsToConsider(), JointStateType.VELOCITY, originalVelocity);
+      input.getRootBody().updateFramesRecursively();
+
+      CommonOps_DDRM.transpose(gradient);
+      return gradient;
+   }
+}
