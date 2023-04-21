f703519d283543f70e5723e350e2294f6603771d
James Foster <james.paul.foster@outlook.com>
2023-04-21 10:48:51 -0500

    First doc pass

--- src/main/java/us/ihmc/mecano/algorithms/JointTorqueRegressorCalculator.java
+++ src/main/java/us/ihmc/mecano/algorithms/JointTorqueRegressorCalculator.java
@@ -1,71 +1,85 @@
 package us.ihmc.mecano.algorithms;
 
+import org.ejml.data.DMatrix;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
 import us.ihmc.mecano.multiBodySystem.interfaces.*;
-import us.ihmc.mecano.spatial.SpatialInertiaParameterBasis;
+import us.ihmc.mecano.spatial.SpatialInertia;
 import us.ihmc.mecano.spatial.interfaces.SpatialInertiaBasics;
+import us.ihmc.mecano.tools.MecanoTools;
 import us.ihmc.mecano.tools.MultiBodySystemTools;
 
 import java.util.*;
 
+/**
+ * Computes the joint torque regressor matrix of a rigid body system based on joint acceleration.
+ * <p>
+ * In this implementation, we calculate the joint torque regressor matrix via repeated application of a modified
+ * Featherstone recursive Newton-Euler method. Focusing on one rigid body, we set the spatial inertia to the parameter to
+ * identify, and set the spatial inertias of all other rigid bodies to zero. Solving these modified inverse dynamics via
+ * recursive Newton-Euler gives the column of the joint torque regressor matrix for that rigid body / parameter combination.
+ * By repeating this over all rigid bodies and parameter combinations, we complete the joint torque regressor matrix.
+ * </p>
+ * <p>
+ * The paper introducing the joint torque regressor matrix is Atkeson, An, & Hollerbach - "Estimation of inertial
+ * parameters of manipulator loads and links" (1986): <a href=https://journals.sagepub.com/doi/10.1177/027836498600500306>link</a>
+ * </p>
+ * <p>
+ * The procedure in this implementation is best described in lecture slides by Wensing - "Optimization-Based Robotics,
+ * AME60621, Lecture 05 - Dynamics Structure and Identification" (2021):
+ * <a href=https://sites.nd.edu/pwensing/teaching/>link</a> [Last accessed 2023-04-21].
+ * </p>
+ * <p>
+ * Note on kinematic loops: this calculator does not support kinematic loops yet.
+ * </p>
+ *
+ * @author James Foster
+ */
 public class JointTorqueRegressorCalculator
 {
     /** Defines the multi-body system to use with this calculator. */
-    private final MultiBodySystemBasics input;
+    private final MultiBodySystemReadOnly input;
 
+    /**
+     * The output of this algorithm: the joint torque regressor matrix that relates the inertial parameters of the
+     * multi-body system to its joint torques in the current state (position, velocity, acceleration of the joints). For
+     * a multi-body system with n DoFs and N rigid bodies, with each rigid body having 10 inertial parameters, the
+     * joint torque regressor matrix will have shape n-by-10N.
+     */
     private final DMatrixRMaj jointTorqueRegressorMatrix;
 
+    /**
+     * A vector containing the inertial parameters of the multi-body system {@link input}. Assuming a multi-body system
+     * consisting of N bodies, with each body having 10 inertial parameters, this vector will be 10N-by-1. This variable
+     * is most useful for testing purposes, especially considering that in theory, we shouldn't have (or need) access to
+     * the inertial parameters in order to generate the joint torque regressor. However, one can multiply the joint
+     * torque regressor by this {@code parameterVector} to see if the torque result matches the result from inverse
+     * dynamics.
+     */
     private final DMatrixRMaj parameterVector;
 
+    /** The common inverse dynamics calculator that is shared between each recursion step of the algorithm. */
     private final InverseDynamicsCalculator inverseDynamicsCalculator;
 
-    private final SpatialInertiaParameterBasis spatialInertiaParameterBasis;
-
+    /** The root of the internal recursive algorithm. */
     private final RecursionStep initialRecursionStep;
 
+    /** Map to quickly retrieve information for each rigid-body. */
     private final Map<RigidBodyReadOnly, RecursionStep> rigidBodyToRecursionStepMap = new LinkedHashMap<>();
 
-    enum SpatialInertiaParameterBasisOptions
-    {
-        M,
-        MCOM_X,
-        MCOM_Y,
-        MCOM_Z,
-        I_XX,
-        I_YY,
-        I_ZZ,
-        I_XY,
-        I_XZ,
-        I_YZ;
-    }
-
-    public SpatialInertiaParameterBasis getBasis(SpatialInertiaParameterBasisOptions basis, RigidBodyBasics rigidBody)
-    {
-        spatialInertiaParameterBasis.setBodyFrame(rigidBody.getInertia().getBodyFrame());
-        spatialInertiaParameterBasis.setReferenceFrame(rigidBody.getInertia().getReferenceFrame());
-        spatialInertiaParameterBasis.setToZero();
-        switch (basis)
-        {
-            case M -> spatialInertiaParameterBasis.setMass(1.0);
-            case MCOM_X -> spatialInertiaParameterBasis.setCenterOfMassOffset(1.0, 0.0, 0.0);
-            case MCOM_Y -> spatialInertiaParameterBasis.setCenterOfMassOffset(0.0, 1.0, 0.0);
-            case MCOM_Z -> spatialInertiaParameterBasis.setCenterOfMassOffset(0.0, 0.0, 1.0);
-            case I_XX -> spatialInertiaParameterBasis.setMomentOfInertia(1.0, 0.0, 0.0);
-            case I_YY -> spatialInertiaParameterBasis.setMomentOfInertia(0.0, 1.0, 0.0);
-            case I_ZZ -> spatialInertiaParameterBasis.setMomentOfInertia(0.0, 0.0, 1.0);
-            case I_XY -> spatialInertiaParameterBasis.setMomentOfInertia(0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
-            case I_XZ -> spatialInertiaParameterBasis.setMomentOfInertia(0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-            case I_YZ -> spatialInertiaParameterBasis.setMomentOfInertia(0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-        }
-        return spatialInertiaParameterBasis;
-    }
-
+    /**
+     * Creates a calculator for computing the joint torque regressor for the given multi-body system {@code input}.
+     * <p>
+     * Do not forget to set the gravitational acceleration so this calculator can properly account for it.
+     * </p>
+     *
+     * @param input the definition of the system to be evaluated by this calculator.
+     * @throws UnsupportedOperationException if the {@code input} contains kinematic loop(s).
+     */
     public JointTorqueRegressorCalculator(MultiBodySystemBasics input)
     {
         this.input = input;
         inverseDynamicsCalculator = new InverseDynamicsCalculator(input);
-        spatialInertiaParameterBasis = new SpatialInertiaParameterBasis();
 
         RigidBodyBasics rootBody = input.getRootBody();
         initialRecursionStep = new RecursionStep(rootBody, null, inverseDynamicsCalculator, null);
@@ -121,17 +135,30 @@ public class JointTorqueRegressorCalculator
         }
     }
 
+    /**
+     * Computes the n-by-10N joint torque regressor matrix for the current state of the multi-body system {@code input},
+     * where n is the number of DoFs and N is the number of rigid bodies in the multi-body system.
+     * <p>
+     * The position, velocity, and acceleration of the joints are extracted directly from the joint state.
+     * </p>
+     */
     public void compute()
     {
         // Set all spatial inertias to zero
         initialRecursionStep.setSpatialInertiasToZeroRecursively();
 
+        // Calculate each column of the regressor, where every column represents a unique rigid body and parameter
+        // combination. By stacking all of these columns together in the next step, we'll get the regressor
         initialRecursionStep.calculateRegressorColumnsRecursively();
 
-        // Collect regressor results for all recursion steps together
+        // Collect regressor results for all recursion steps
         collectJointTorqueRegressorFromRecursionSteps();
     }
 
+    /**
+     * Assuming all the recursion steps of the algorithm have been run through, this method will collect the results
+     * into the overall joint torque regressor matrix.
+     */
     public void collectJointTorqueRegressorFromRecursionSteps()
     {
         int i = 0;
@@ -162,26 +189,70 @@ public class JointTorqueRegressorCalculator
         }
     }
 
+    /**
+     * Set the gravitational acceleration to account for in the multi-body system {@code input}.
+     * <p>
+     * The acceleration of the root body is set to the opposite of the gravitational acceleration such
+     * that it gets naturally propagated to the whole system.
+     * </p>
+     *
+     * @param gravity the gravitational linear acceleration along the z-axis, it is usually equal to
+     *                {@code -9.81}.
+     */
     public void setGravitationalAcceleration(double gravity)
     {
         setGravitationalAcceleration(0.0, 0.0, gravity);
     }
 
+    /**
+     * Set the gravitational acceleration to account for in the multi-body system {@code input}.
+     * <p>
+     * The acceleration of the root body is set to the opposite of the gravitational acceleration such
+     * that it gets naturally propagated to the whole system.
+     * </p>
+     *
+     * @param gravityX the gravitational linear acceleration along the x-axis, it is usually equal to
+     *                 {@code 0}.
+     * @param gravityY the gravitational linear acceleration along the y-axis, it is usually equal to
+     *                 {@code 0}.
+     * @param gravityZ the gravitational linear acceleration along the z-axis, it is usually equal to
+     *                 {@code -9.81}.
+     */
     public void setGravitationalAcceleration(double gravityX, double gravityY, double gravityZ)
     {
         inverseDynamicsCalculator.setGravitionalAcceleration(gravityX, gravityY, gravityZ);
     }
 
+    /**
+     * Gets the 10-by-1 inertial parameter vector of the multi-body system {@code input}, where N is the number of rigid
+     * bodies in the multi-body system.
+     * <p>
+     * In general, this should mostly be used for debugging purposes, and if called, should be called soon after the
+     * constructor, else the result wil be meaningless (e.g. set to all zeros, or all zeros with a few ones).
+     * </p>
+     * <p>
+     * For more details, see {@link parameterVector}.
+     * </p>
+     *
+     * @return the 10N-by-1 vector of inertial parameters.
+     */
     public DMatrixRMaj getParameterVector()
     {
         return parameterVector;
     }
 
+    /**
+     * Gets the computed n-by-10N joint torque regressor matrix of the multi-body system {@code input}, where n is the
+     * number of DoFs and N is the number of rigid bodies in the multi-body system.
+     *
+     * @return the n-by-10N joint torque regressor matrix.
+     */
     public DMatrixRMaj getJointTorqueRegressorMatrix()
     {
         return  jointTorqueRegressorMatrix;
     }
 
+    /** Utility method for converting SpatialInertia objects to matrix form. */
     private static DMatrixRMaj spatialInertiaToParameterVector(SpatialInertiaBasics spatialInertia)
     {
         DMatrixRMaj parameters = new DMatrixRMaj(10, 1);
@@ -198,6 +269,11 @@ public class JointTorqueRegressorCalculator
         return parameters;
     }
 
+    /**
+     * A {@code SpatialInertia} object is linear with respect to the ten inertial parameters that constitute it.
+     * Therefore, we can create a basis for all spatial inertias with respect to these parameters. This enum is used in
+     * {@code SpatialInertiaParameterBasis} to generate these bassis matrices.
+     */
     enum SpatialInertiaParameterBasisOptions
     {
         M,
@@ -212,14 +288,36 @@ public class JointTorqueRegressorCalculator
         I_YZ;
     }
 
+    /**
+     * A {@code SpatialInertia} object is linearly parameterised with respect to the ten inertial parameters that
+     * constitute it. Therefore, we can create a basis for all spatial inertias with respect to these parameters. This
+     * class is a simple wrapper around {@code SpatialInertia} that facilitates generation of the spatial inertia basis
+     * matrices, usually by the user providing a desired basis in the form of {@code SpatialInertiaParamaterBasisOptions}.
+     * <p>
+     * Note: as they are bases of the set of spatial inertias, the basis matrices are nearly always non-physical.
+     * </p>
+     *
+     * @author James Foster
+     */
     static class SpatialInertiaParameterBasis extends SpatialInertia
     {
+        /**
+         * The basis matrix is initialised as a zero spatial inertia, inheriting the reference frames of the input
+         * rigid body.
+         *
+         * @param rigidBody the rigid body for which a spatial inertia basis is to be constructed.
+         */
         public SpatialInertiaParameterBasis(RigidBodyBasics rigidBody)
         {
             super(rigidBody.getInertia().getBodyFrame(), rigidBody.getInertia().getReferenceFrame());
             setToZero();
         }
 
+        /**
+         * Set the spatial inertia to the basis specified by the chosen {@code SpatialInertiaParameterBasisOptions}.
+         *
+         * @param basisOption the basis to set the spatial inertia to.
+         */
         public void setBasis(SpatialInertiaParameterBasisOptions basisOption)
         {
             setToZero();
@@ -244,6 +342,13 @@ public class JointTorqueRegressorCalculator
             get(0, 0, matrixToPack);
         }
 
+        /**
+         * This override is subtly different to the original implementation. The original implementation's
+         * functionality enforces that the first moments of mass in the upper-right and lower-left blocks of the spatial
+         * inertia are coupled to the diagonal matrix block of mass in the lower-right corner, as is correct for
+         * physically realisable spatial inertias. However, for the physically unrealisable
+         * {@code SpatialInertiaParameterBasis} objects needed here, this coupling must be removed.
+         * */
         @Override
         public void get(int startRow, int startColumn, DMatrix matrixToPack)
         {
@@ -268,21 +373,51 @@ public class JointTorqueRegressorCalculator
         }
     }
 
+    /**
+     * Represents a single recursion step for the two passes needed for regressor calculation for a rigid body.
+     *
+     * @author James Foster
+     */
     private class RecursionStep
     {
+        /** The rigid body belonging to this recursion step. */
         private final RigidBodyBasics rigidBody;
 
+        /** The recursion step holding onto the direct predecessor of this recursion step's rigid body. */
         private final RecursionStep parent;
 
+        /**
+         * A list containing the recursion steps holding onto the direct successors of this recursion step's
+         * rigid body.
+         */
         private final List<RecursionStep> children = new ArrayList<>();
 
+        /**
+         * The inverse dynamics calculator used for regressor calculation.
+         * <p>
+         * Note: this calculator is shared throughout the whole algorithm, including all of the recursion steps. As
+         * such, be vary wary of arbitrarily changing its state.
+         * </p>
+          */
         private InverseDynamicsCalculator inverseDynamicsCalculator;
 
+        /**
+         * Intermediate variable responsible for holding on to the spatial inertia parameter basis currently being used
+         * for other calculations.
+         */
         private SpatialInertiaParameterBasis spatialInertiaParameterBasis;
 
-        /** Temporary variable to hold results of a column of the regressor matrix */
+        /**
+         * Intermediate variable to hold a column of the regressor matrix. This column will correspond to a
+         * {@code rigidBody} and {@code SpatialInertiaParameterBasisOption} combination. It is an n-by-1 vector, where n
+         * is the number of DoFs of the multi-body system {@code input}.
+         */
         private DMatrixRMaj regressorColumn;
 
+        /**
+         * Intermediate variable the block of the regressor matrix corresponding to this {@rigidBody}. It is an n-by-10
+         * matrix, where n is the number of DoFs of the multi-body system {@code input}.
+         */
         private DMatrixRMaj regressorMatrixBlock;
 
         public RecursionStep(RigidBodyBasics rigidBody, RecursionStep parent, InverseDynamicsCalculator inverseDynamicsCalculator, int[] jointsToIgnore)
@@ -301,7 +436,7 @@ public class JointTorqueRegressorCalculator
             }
         }
 
-        /** First sweep sets all spatial inertias to zero */
+        /** First pass: set the spatial inertias of all the rigid bodies to zero. */
         public void setSpatialInertiasToZeroRecursively()
         {
             // Only make changes if the rigid body of concern isn't a null body like the root / elevator
@@ -315,9 +450,9 @@ public class JointTorqueRegressorCalculator
             }
         }
 
-        /** Second pass, assuming that all spatial inertias have been zeroed in the first sweep (therefore there are
-         * no spatial inertias earlier in the tree that we have to care about). Here, we pass in a basis and set the
-         * spatial inertia of the current rigid body
+        /**
+         * Second pass: for each rigid body, iterate over and perform inverse dynamics for all the spatial inertia
+         * parameter bases.
          */
         public void calculateRegressorColumnsRecursively()
         {
@@ -329,10 +464,14 @@ public class JointTorqueRegressorCalculator
                     // Set spatial inertia of this rigid body to be the desired basis
                     spatialInertiaParameterBasis.setBasis(basis);
                     rigidBody.getInertia().set(spatialInertiaParameterBasis);
+
+                    // Perform inverse dynamics calculation and store the result
                     inverseDynamicsCalculator.getBodyInertia(rigidBody).set(spatialInertiaParameterBasis);
                     inverseDynamicsCalculator.compute();
                     regressorColumn.set(inverseDynamicsCalculator.getJointTauMatrix());
                     setRegressorMatrixColumn(regressorColumn, basis);
+
+                    // Set the spatial inertia of the rigid body back to zero for subsequent iterations / recursions
                     rigidBody.getInertia().setToZero();
                     inverseDynamicsCalculator.getBodyInertia(rigidBody).setToZero();
                 }
@@ -342,6 +481,19 @@ public class JointTorqueRegressorCalculator
             }
         }
 
+        /**
+         * Utility method for inserting a calculated column (vector) of the regressor corresponding to a chosen
+         * {@code SpatialInertiaParameterBasisOptions} to the appropriate place in this recursion step's
+         * {@code regressorMatrixBlock}.
+         * <p>
+         * The block of the joint torque regressor corresponding to a given rigid body will have ten columns. We choose
+         * to order them as they are ordered in the enum {@link SpatialInertiaParameterBasisOptions}.
+         * </p>
+         *
+         * @param regressorColumn the n-by-1 vector representing the column for parameter {@code basis}.
+         * @param basis the {@code SpatialInertiaParameterBasisOptions} parameter basis, which informs the ordering and
+         *              placement of regressor column entries.
+         */
         public void setRegressorMatrixColumn(DMatrixRMaj regressorColumn, SpatialInertiaParameterBasisOptions basis)
         {
             CommonOps_DDRM.insert(regressorColumn, regressorMatrixBlock, 0, basis.ordinal());
