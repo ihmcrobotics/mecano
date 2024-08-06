cb0fb6ddb2510d6b42d2ad3d65d5e72c4ba9594f
Robert Griffin <rgriffin@ihmc.us>
2024-08-06 07:47:28 -0500

    deleted minimum jerk trajectory and deprecated trapesoidal velocity trajectory and its implementations. Hexapod still depends on it.
    delete matrix yo variable converter tools
    
    delete some old yo variable tools
    
    delete some unused yo variable tools
    
    delete unused stiction model tools
    
    delete unused alpha to alpha functions
    
    deleted some controller classes, and left others to be reviewed
    
    deprecated some straght line tests, they need to be pushed into the striaght line pose traejctory
    
    removed straight line poisition trajectory from the pose test
    
    removed the straight line independent trajectory generators that werent used
    
    deleted the unused qp code
    
    deleted projection tools
    
    started switching away from random geometry class and the old robotics assert
    
    converted more of the tests to the right format
    
    switched getting all the current crop of moving away from random geometry working
    
    got rid of the custom random matrix generator class
    
    deleted the random geometry class, since its duplicated by other tools
    
    fixed some deletes and removed some provider structure that did nothing
    
    fixed some more deletes
    
    moved the planar landmark to the correct place
    
    deleted many other classes
    
    delete unused code
    
    deleted a whole bunch of stuff from the java toolkit
    
    deleted the simple walking controller

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/MultiBodySystemMissingTools.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/MultiBodySystemMissingTools.java
@@ -89,4 +89,22 @@ public class MultiBodySystemMissingTools
          }
       };
    }
+
+   /**
+    * Computes the mass of all the links contained in the subtree of {@param rootBody}
+    * @param rootBody
+    * @return subtree mass
+    */
+   public static double computeSubTreeMass(RigidBodyReadOnly rootBody)
+   {
+      SpatialInertiaReadOnly inertia = rootBody.getInertia();
+      double ret = inertia == null ? 0.0 : inertia.getMass();
+
+      for (int i = 0; i < rootBody.getChildrenJoints().size(); i++)
+      {
+         ret += computeSubTreeMass(rootBody.getChildrenJoints().get(i).getSuccessor());
+      }
+
+      return ret;
+   }
 }
