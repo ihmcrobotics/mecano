dbdd4de0f2b7dcfbb5bdd1c1cfc4518b19a29201
Dexton Anderson <danderson@ihmc.org>
2025-02-05 10:34:15 -0600

    :arrow_up: javafx 17.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ testDependencies {
 
    api("us.ihmc:log-tools:0.6.5")
 
-   var javaFXVersion = "17.0.2"
+   var javaFXVersion = "17.0.8"
    api(ihmc.javaFXModule("base", javaFXVersion))
    api(ihmc.javaFXModule("controls", javaFXVersion))
    api(ihmc.javaFXModule("graphics", javaFXVersion))
