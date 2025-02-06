a174e568074b2e692fefa3a326f8857f73af66a6
Dexton Anderson <danderson@ihmc.org>
2025-02-06 11:52:47 -0600

    :arrow_up: ihmc-yovariables 0.13.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,11 +46,11 @@ graphvizDependencies {
 yovariablesDependencies {
    api(ihmc.sourceSetProject("main"))
 
-   api("us.ihmc:ihmc-yovariables:0.13.5")
+   api("us.ihmc:ihmc-yovariables:0.13.6")
 }
 
 yovariablesFiltersDependencies {
    api(ihmc.sourceSetProject("yovariables"))
 
-   api("us.ihmc:ihmc-yovariables-filters:0.13.5")
+   api("us.ihmc:ihmc-yovariables-filters:0.13.6")
 }
